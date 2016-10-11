399f5d39a5053be17b2811639ee224540a577154
beckend <linken.dinh@gmail.com>
2016-10-11 17:18:54 +0200

    bump

--- src/combineEpics.ts
+++ src/combineEpics.ts
@@ -3,13 +3,6 @@ import { Observable } from 'rxjs/Observable';
 import { IEpic } from './model';
 import { Action } from 'redux-actions';
 
-export interface IGenericEpic {
-  (...args: any[]): Observable<any>;
-}
-
-/* tslint:disable:max-line-length */
-// export function combineEpics<TAction extends Action<any>, TStoreState>(...epics: IEpic<TAction, TStoreState>[]): (...args: any[]) => Observable<TAction>;
-
 /**
  * Merges all epics into a single one.
  */
