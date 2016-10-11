c4c1392badd95de68513881544ed7d27f80ac4fe
beckend <linken.dinh@gmail.com>
2016-10-11 17:16:26 +0200

    remove junk

--- etc/docker/fs/root/scripts/zsh/install.sh
+++
@@ -1,8 +0,0 @@
-# Install zsh
-apt-get install zsh -y
-
-# Install prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
-# Set zsh as default
-chsh -s /bin/zsh
