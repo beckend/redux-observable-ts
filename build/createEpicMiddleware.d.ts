c454b4a721278de7ef900c110baf11e4083fd37a
Linken Dinh <linken.dinh@gmail.com>
2016-11-27 17:37:47 +0100

    delete build folder, update main to es5 build folder

--- build/createEpicMiddleware.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="redux-actions" />
-import { Action } from 'redux-actions';
-import { IEpicAdapter, IEpic, IEpicMiddleware } from './model';
-import './rxjs/add/__invoke';
-export interface IDefaultOptions {
-    adapter: IEpicAdapter;
-}
-export declare function createEpicMiddleware<TAction extends Action<any>, TStoreState>(epic: IEpic<TAction, TStoreState>, {adapter}?: IDefaultOptions): IEpicMiddleware<TAction, TStoreState>;
